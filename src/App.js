964aad0833ca38a9c6a85654f31a2b583a109069
Nader <Nader.Hassouni@Hotmail.com>
2020-06-06 01:53:52 +0200

    finished project week2

--- src/App.js
+++ src/App.js
@@ -1,33 +1,52 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import WeatherCard from "./component/WeatherCard";
+import Button from "./component/Button";
+import Input from "./component/Input"
 
-const data = require("./city-weather.json");
 
-class App extends Component {
-  render() {
+const App = () => {
+  const apiKey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
+  const [cityName, setCityName] = useState('');
+  const [cityData, setCityData] = useState({});
+  const [error, setError] = useState(false)
+  const [isLoading,setIsLoading] = useState(false)
+  const getCityWeather = () => {
+    setIsLoading(true)
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>{
+      if(data.cod === '400' ||data.cod === '404'){
+        setError(true)
+        setIsLoading(false);
+      }else{
+        setCityData({...data});
+        setError(false)
+        setIsLoading(false);
+      }
+    })
+    .catch(error =>{ 
+      setError(true)
+      setIsLoading(false) 
+    })
+  }
+  const onChangeInput = (val)=> {
+    setCityName(val)
+  }
+  
+  
 
     return (
     <div className="App">
       <h1>Weather</h1>
-    {data.map(city=> {
-      return(
-        <WeatherCard
-        key={city.id}
-        cityName={city.name}
-        countryName={city.sys.country}
-        weatherMain={city.weather[0].main}
-        weatherDescription={city.weather[0].description}
-        maxTemp={city.main.temp_max}
-        minTemp={city.main.temp_min}
-        lon={city.coord.lon}
-        lat={city.coord.lat}
-        />
-      )
-    })}
+      <Input onChange={onChangeInput} val={cityName}></Input>
+      <Button onClick={getCityWeather}>Search</Button>
+      {isLoading && !error ? <p>Loading ...</p>: null}
+      {!isLoading && cityData && !error ? <WeatherCard data={cityData}/> : null}
+      {error && <p className="errorMessage">Please enter a valid city name!</p>}
     </div>
     )
-  }
 }
 
 export default App;
--- src/component/WeatherCard.js
+++ src/component/WeatherCard.js
@@ -2,16 +2,20 @@ import React from 'react';
 import "./Weathercard.css";
 
 const WeatherCard = (props) => {
-    console.log(props);
-    
+    console.log(props)
     return(
 <div className='card'>
-    <h1>{props.cityName}, {props.countryName}</h1>
-    <h3>{props.weatherMain}</h3>
-    <p>{props.weatherDescription}</p>
-    <p>min temp: {props.minTemp}</p>
-    <p>max temp: {props.maxTemp}</p>
-    <p>location: {props.lon},{props.lat}</p>
+    {Object.keys(props.data).length > 0 ?
+    (<div>
+    <h2>{props.data.name}, {props.data.sys.country}</h2>
+    <h3>{props.data.weather[0].main}</h3>
+    <p>{props.data.weather[0].description}</p>
+    <p>min temp: {props.data.main.temp_min}</p>
+    <p>max temp: {props.data.main.temp_max}</p>
+    <p>location: {props.data.coord.lon},{props.data.coord.lat}</p> 
+    </div>):
+    <div>write a city name</div>
+}
 </div>
     )
 }
